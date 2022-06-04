94c94ab82267cea7f8596fa7b69df2f2d13d01b4
ominyoung <ohmj090747@gmail.com>
2022-06-05 02:02:52 +0900

    ejs layouts module

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const expressLayouts = require('express-ejs-layouts');
 const express = require('express');
 const bodyparser = require('body-parser');
 const path = require('path');
@@ -8,10 +9,19 @@ const port = 3000;
 
 // body-parser와 ejs 적용
 app.use(bodyparser.urlencoded({extended : true}))
-app.set('view engine','ejs')
+app.set('view engine','ejs');
+app.set('views',__dirname + '/views');
+
+//views/index.ejs를 기본 레이아웃으로 정한다.
+app.set('layout','index');
+
+app.use(expressLayouts);
 
 app.get('/',(req, res)=>{
-    res.render('index');
+    res.render('body/home');
+})
+app.get('/outfit',(req, res)=>{
+    res.render('body/outfit');
 })
 
 // 정적 파일
